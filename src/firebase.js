48ca20dd3702ee2328f0f5ba00b3ec8ba9094ff9
Vinod Jarare <vsjarare@gmail.com>
2023-02-23 14:10:54 +0530

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCTH075q6b_nDL7Lct3V3MDwMQf0dNuccA",
+  authDomain: "chat-721b7.firebaseapp.com",
+  projectId: "chat-721b7",
+  storageBucket: "chat-721b7.appspot.com",
+  messagingSenderId: "189373603497",
+  appId: "1:189373603497:web:db837c150bab8885e013b8",
 };
 
 // Initialize Firebase
